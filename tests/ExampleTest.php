a2565d6930ee9a860a48cfa757f65325bad6d8c2
David Wright <dave@ritey.com>
2025-10-04 09:38:51 +0100

    Add ignore and scaffolding for tests

---
+++ tests/ExampleTest.php
@@ -0,0 +1,16 @@
+<?php
+
+use PHPUnit\Framework\TestCase;
+
+/**
+ * @internal
+ *
+ * @coversNothing
+ */
+class ExampleTest extends TestCase
+{
+    public function testHelloWorld()
+    {
+        $this->assertEquals('Hello, World!', 'Hello, World!');
+    }
+}
